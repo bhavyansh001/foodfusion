e60c72a42e8a453219e128f503142b216926d044
Bhavyansh <bhavyansh001@gmail.com>
2024-08-10 11:53:15 +0530

    feat: Add ordering functionality
    - Modified OrdersController to handle new order creation and display
    - Updated restaurants show view to include order-related information
    - Updated routes to include orders resource
    - Added show view for orders to display order details

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
-  resources :restaurants
+  resources :restaurants do
+    post '/add_order', to: 'orders#add_order'
+    resources :menu_items
+  end
+  resources :orders, only: [:show]
   get "up" => "rails/health#show", as: :rails_health_check
 end
