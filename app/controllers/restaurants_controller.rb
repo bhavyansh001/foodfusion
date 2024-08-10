364bf298bd597061951b62e4ef08451daabbeae8
Bhavyansh <bhavyansh001@gmail.com>
2024-08-10 22:54:58 +0530

    feat: Add CRUD functionality for Restaurants
    - Modified restaurants_controller.rb to support restaurant CRUD operations.
    - Updated views for restaurant CRUD:
      - Modified index.html.erb and show.html.erb for owner dashboard views.
      - Added new.html.erb for creating new restaurants.
      - Modified edit.html.erb for editing restaurant details.
      - Updated _form.html.erb partial to handle form inputs for restaurant creation and updates.
      - Deleted old index.html.erb under restaurants views.
    - Update orders_controller.rb to authenticate user before ordering

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,20 @@
 class RestaurantsController < ApplicationController
-  def index
-  end
-
+  before_action :authenticate_user!, except: [:show]
+  before_action :ensure_owner, except: [:show]
+  before_action :set_restaurant_instance,
+                 only: [:edit, :update, :destroy]
   def new
+    @restaurant = current_user.restaurants.build
   end
 
   def create
+    @restaurant = current_user.restaurants.build(restaurant_params)
+    if @restaurant.save
+      redirect_to owner_dashboard_index_path,
+        notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
   end
 
   def show
@@ -13,12 +22,34 @@ class RestaurantsController < ApplicationController
     @menu_items = @restaurant.menu_items
   end
 
-  def edit
-  end
+  def edit;  end
 
   def update
+    if @restaurant.update(restaurant_params)
+      redirect_to owner_dashboard_index_path,
+        notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @restaurant.destroy
+    redirect_to owner_dashboard_index_path,
+      notice: 'Restaurant was successfully deleted.'
+  end
+
+  private
+
+  def set_restaurant_instance
+    @restaurant = current_user.restaurants.find(params[:id])
+  end
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
+  def ensure_owner
+    redirect_to root_path,
+      alert: 'Access denied.' unless current_user.owner?
   end
 end
