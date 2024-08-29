da7ee1a8b489abce79dc30610471b06fdf522bc9
Bhavyansh <bhavyansh001@gmail.com>
2024-08-29 13:49:45 +0530

    feat: Add internationalization for more pages - orders/show and order_mailer/status_update.html.erb

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,6 @@
 class OrderMailer < ApplicationMailer
   def status_update(order)
     @order = order
-    mail(to: @order.visitor.email, subject: "Order Status Update")
+    mail(to: @order.visitor.email, subject: I18n.t('email.order_status.subject'))
   end
 end
