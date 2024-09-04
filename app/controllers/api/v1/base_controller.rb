c3d71527dbae29aace6acc65c4bba4c23f161e2a
Bhavyansh <bhavyansh001@gmail.com>
2024-09-04 15:21:15 +0530

    refactor: Optimize API controllers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,7 +9,8 @@ module Api
         token = request.headers['Authorization']&.split(' ')&.last
         @current_user = User.find_by(api_token: token)
 
-        render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+        render json: { error: 'Unauthorized' },
+               status: :unauthorized unless @current_user
       end
 
       attr_reader :current_user
