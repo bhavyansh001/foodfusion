9c47786ae977fbc001de528182d5d37ac964b224
Bhavyansh <bhavyansh001@gmail.com>
2024-09-16 13:42:08 +0530

    hotfix: Use docker network for security

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :solid_queue
@@ -107,7 +108,7 @@ Rails.application.configure do
     port: 587,
     domain: 'foodfusion.diversepixel.com',
     user_name: 'hi@foodfusion.diversepixel.com',
-    password: ENV["SMTP_PASSWORD"],
+    password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true,
     open_timeout: 5,
